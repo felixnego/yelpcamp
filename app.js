9d01468e37dbf2f19eb387f8e7704a22c8f14a70
Felix Negoita <felixnego@gmail.com>
2020-04-27 15:14:34 +0300

    installed the auth packages and added passport plugin to the user model

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const localStrategy = require('passport-local')
 const Campground = require('./models/campground')
 const Comment = require('./models/comment')
+const User = require('./models/user')
 const seedDB = require('./seeds')
 const app = express()
+
 
 
 app.set('view engine', 'ejs')
--- models/user.js
+++ models/user.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
     email: String, 
     name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }
-    ]
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 const User = mongoose.model('User', userSchema)
 
